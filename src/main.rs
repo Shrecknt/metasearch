5d31074d7e7ca25a7d685ebd6cc1988c3a4c61a3
Shrecknt <58538423+Shrecknt@users.noreply.github.com>
2024-03-18 23:58:42 -0700

    add acceptable words list

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,10 @@ async fn main() {
         for word in ADDITIONAL_PROFANE_WORDS {
             rustrict::Trie::customize_default().set(word, rustrict::Type::INAPPROPRIATE);
         }
+        const ADDITIONAL_ACCEPTABLE_WORDS: &[&str] = &["shrecknt"];
+        for word in ADDITIONAL_ACCEPTABLE_WORDS {
+            rustrict::Trie::customize_default().set(word, rustrict::Type::SAFE);
+        }
     }
 
     web::run().await;
